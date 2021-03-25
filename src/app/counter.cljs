4bbf9bfc80b973ba95d06026ac3f90ed9fde8daf
Nazeh <ar.nazeh@gmail.com>
2021-03-25 09:32:11 +0300

    Fix counter and refactor tem_converter

--- src/app/counter.cljs
+++ src/app/counter.cljs
@@ -5,6 +5,6 @@
   [wrapper {:title "Counter"}
     [:div.row
      [:p "Count:"]
-     [:input.field {:defaultValue @click-count}]]
+     [:input.field {:value @click-count :readOnly true}]]
     [:input {:type "button" :value "Increment"
              :on-click #(swap! click-count inc)}]])
--- src/app/temp_converter.cljs
+++ src/app/temp_converter.cljs
@@ -2,7 +2,10 @@
   (:require [reagent.core :as r]
             [app.wrapper :refer [wrapper]]))
 
-(def default-state {:val "" :err ""})
+(defn numeric?
+"Check that input string is a valid Number"
+  [string]
+  (not (js/Number.isNaN (js/Number string))))
 
 (defn sci-format
 "Convert to the scientific notation if the number is >= 10^12"
@@ -13,22 +16,30 @@
 
 (defn c-to-f
 "Contvert celsius to fahrenheit"
-  [temp]
-  (sci-format (Math/ceil (+ 32 (* temp (/ 9 5))))))
+  [c]
+  (+ 32 (* c (/ 9 5))))
+
 (defn f-to-c
 "Contvert fahrenheit to celsius "
-  [temp]
-  (sci-format(Math/floor  (* (- temp 32) (/ 5 9)))))
+  [f]
+  (* (- f 32) (/ 5 9)))
 
-(defn validNumber?
-"Check that input string is a valid Number"
-  [string]
-  (not (js/Number.isNaN (js/Number string))))
+(defn convert
+  "Convert temprature according to target unit"
+  [unit temp]
+  (->
+   (case unit
+     :fah (c-to-f temp)
+     :cel (f-to-c temp))
+   Math/round
+   sci-format))
 
-(defn freeze 
-"Set the :err in a target state to 'freeze'"
+(defn disable 
+"Set the :err in a target state to 'disable'"
   [state key]
-  (swap! state assoc-in [key :err] "freeze"))
+  (swap! state assoc-in [key :err] "disable"))
+
+(def default-state {:val "" :err ""})
 
 (defn on-change
   "Mutate the state atom of the temperature converter according to passed key"
@@ -36,20 +47,18 @@
   (let [new-val (-> e .-target .-value)
         other-key (case key
                     :cel :fah
-                    :fah :cel)
-        convert-func (case other-key
-                       :fah c-to-f
-                       :cel f-to-c)]
-    (if (=  new-val "")
+                    :fah :cel)]
+    (if (= new-val "")
       (do (swap! state assoc key default-state)
-          (freeze state other-key))
-      (if (validNumber? new-val)
-      (do 
-        (swap! state assoc key {:val new-val :err ""})
-        (swap! state assoc other-key {:val (convert-func new-val) :err ""}))
+          (disable state other-key))
+      (if (numeric? new-val)
         (do
-          (freeze state other-key)
-          (swap! state assoc key {:val new-val :err "invalid"}))))))
+          (swap! state assoc key {:val new-val :err ""})
+          (swap! state assoc other-key
+                 {:val (convert other-key new-val) :err ""}))
+        (do
+          (swap! state assoc key {:val new-val :err "invalid"})
+          (disable state other-key))))))
 
 (defn converter []
   (let [state (r/atom {:cel default-state :fah default-state})]
