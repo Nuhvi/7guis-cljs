c3dd1680b072d4ff64f6e573b7e4669eb55d7241
Nazeh <ar.nazeh@gmail.com>
2021-05-05 10:04:22 +0300

    Massive refactor for the CRUD component

--- src/app/crud.cljs
+++ src/app/crud.cljs
@@ -2,11 +2,24 @@
   (:require [reagent.core :as r]
             [app.wrapper :refer [wrapper]]))
 
-;; ============================================
-;; Global state to simulate an external backend
-;; ============================================
+;; ==========
+;; Generators
+;; ==========
 
-(defonce users (r/atom []))
+(defn initial-state
+  "Generate the intial state of the crud component,
+   given an intial vector of users"
+  [users]
+  {:name ""
+   :surname ""
+   :prefix ""
+   :users users
+   :selected (:id (first users))})
+
+(defn fullname
+  "Generate fullname from user's name and surname"
+  [user]
+  (str (:surname user) ", " (:name user)))
 
 (defn generate-user
   "Generate a user using name, surname and id,
@@ -16,137 +29,171 @@
    :name name
    :surname surname})
 
-(defn create-user!
-  "Create a new user, and return the new state of users"
-  [name surname]
-  (swap! users conj (generate-user {:name name :surname surname})))
+;; ========
+;; Checkers
+;; ========
 
-;; Generate initial data
-(defonce initial-data
-  (do (create-user! "Hans" "Emil")
-      (create-user! "Max" "Mustermann")
-      (create-user! "Roman" "Tisch")))
-initial-data
-
-(defn find-index
-  "Find the first index where the predicate returns true"
-  [pred coll]
-  (first (keep-indexed #(if (pred %2) %1 nil) coll)))
-
-(defn update-user!
-  "Update a given user by id, and return the new state of db"
-  [id name surname]
-  (swap! users assoc
-         (find-index #(= (:id %) id) @users)
-         (generate-user {:id id :name name :surname surname})))
-
-(defn delete-user!
-  "Delete a given user by id, and return the new sate of db"
-  [id]
-  (swap! users (fn [coll] (filterv #(not= (:id %) id) coll))))
-
-;; ===================
-;; Component functions
-;; ===================
-
-(defn invalid-input?
-  "Check if the name or surname are empty"
+(defn valid-input?
+  "Check if both the name and surname are not empty"
   [state]
-  (or (zero? (count (:name state)))
-      (zero? (count (:surname state)))))
+  (and (seq (:name state))
+       (seq (:surname state))))
 
-(defn nothing-selected?
-  "Checks if the state has an active id"
+(defn has-selected?
+  "Checks if the state has a selected id"
   [state]
-  (zero? (count (:selected state))))
+  (seq (:selected state)))
 
-(defn fullname
-  "Generate fullname from user's name and surname"
-  [user]
-  (str (:surname user) ", " (:name user)))
+(defn has-no-prefix?
+  "check if the state has no perfix input for filtering"
+  [state]
+  (not (seq (:prefix state))))
 
-(defn filter-list
+(defn can-create?
+  "Check if the state has no prefix,
+   but contains valid name and surname"
+  [state]
+  (and (valid-input? state)
+       (has-no-prefix? state)))
+
+(defn can-update?
+  "Check if the state has an active selection, 
+   no prefix and valid name and surname"
+  [state]
+  (and (has-selected? state)
+       (valid-input? state)
+       (has-no-prefix? state)))
+
+(defn can-delete?
+  "Check if the state has an active selection,
+   and no prefix"
+  [state]
+  (and (has-selected? state)
+       (has-no-prefix? state)))
+
+;; ==============
+;; vector helpers
+;; ==============
+
+(defn find-pos
+  "Find the position of a user in a vector by their uuid"
+  [id coll]
+  (first (keep-indexed #(when (= (:id %2) id) %1) coll)))
+
+(defn filter-users
   "Filter a list of users using filter prefix"
-  [list ^string prefix]
-  (filter #(re-find (re-pattern (str "(?i)" prefix)) (fullname %)) list))
+  [users ^string prefix]
+  (filter #(re-find (re-pattern (str "(?i)" prefix)) (fullname %)) users))
+
+(defn vec-remove
+  "Remove an item from a vector at a given index"
+  [pos coll]
+  (vec (concat (subvec coll 0 pos) (subvec coll (inc pos)))))
+
+(defn find-nearest
+  "Find the nearest item in a collection from a given position"
+  [pos coll]
+  (if (>= (count coll) (inc pos))
+    (coll pos)
+    (last coll)))
+
+;; ==============
+;; State modifers
+;; ==============
+
+(defn set-target-value
+  "Set the value of a given key in the state to the event target value"
+  [state key event]
+  (swap! state assoc key (.. event -target -value)))
 
 (defn handle-create!
-  "Create new user and switch focus to it"
+  "Create new user and switch selected its uuid"
   [state]
-  (let [old-state @state
-        name (:name old-state)
-        surname (:surname old-state)]
-    (->> (create-user! name surname)
-         last
-         :id
-         (swap! state assoc :selected))))
+  (let [s @state
+        new-user (generate-user {:name (:name s) :surname (:surname s)})]
+    (when (can-create? s))
+    (swap! state assoc
+           :users (conj (:users s) new-user)
+           :selected (:id new-user))))
 
 (defn handle-update!
   "Update the selected user with name and surname"
   [state]
-  (let [old-state @state
-        id (:selected old-state)
-        name (:name old-state)
-        surname (:surname old-state)]
-    (update-user! id name surname)))
+  (let [s @state
+        id (:selected s)
+        name (:name s)
+        surname (:surname s)
+        position (find-pos (:selected s) (:user s))]
+    (when (can-update? s)
+      (swap! state assoc-in
+             [:users position]
+             (generate-user {:id id :name name :surname surname})))))
 
 (defn handle-delete!
-  ;; TODO make it switch to the closest users in the list instead
   "Delete an user and switch focus to the first user"
   [state]
-  (let [new-users (delete-user! (:selected @state))]
-    (swap! state assoc :selected (:id (first new-users)))))
+  (let [s @state
+        position (find-pos (:selected s) (:users s))
+        new-users (vec-remove position (:users s))]
+    (when (can-delete? s)
+      (swap! state assoc
+             :users new-users
+             :selected (:id (find-nearest position new-users))))))
 
+;; ==========
+;; Components
+;; ==========
 
 (defn listbox
   "Renders the listbox using a given list of users"
   [{:keys [list value on-change]}]
   [:div.row [:select.field.full-width
-             {:value value
+             {:value (or value "")
               :size 3
               :on-change on-change}
              (for [user list]
                [:option
                 {:value (:id user) :key (:id user)}
                 (fullname user)])]])
+            
+(defonce DEMO_DATA [(generate-user {:name "Hans" :surname "Emil"})
+                    (generate-user {:name "Max" :surname "Mustermann"})
+                    (generate-user {:name "Tisch" :surname "Roman"})])
 
-(defn crud []
-  (let [state (r/atom {:name ""
-                       :surname ""
-                       :prefix ""
-                       :selected (:id (first @users))})]
+(defn crud [users]
+  (let [state (r/atom (initial-state (or users DEMO_DATA)))]
     (fn []
+      (js/console.log (clj->js @state))
       [wrapper {:title "CRUD"}
        [:div.row
         [:p "Filter prefix"]
         [:input.field
-         {:on-change #(swap! state assoc :prefix (.. % -target -value))}]]
+         {:on-change #(set-target-value state :prefix %)}]]
        [listbox
-        {:list (filter-list @users (:prefix @state))
+        {:list (filter-users (:users @state) (:prefix @state))
          :value (:selected @state)
-         :on-change #(swap! state assoc :selected (.. % -target -value))}]
+         :on-change #(set-target-value state :selected %)}]
        [:div.row
         [:p "Name"]
         [:input.field
-         {:on-change #(swap! state assoc :name (.. % -target -value))}]]
+         {:on-change #(set-target-value state :name %)}]]
        [:div.row
         [:p "Surname"]
         [:input.field
-         {:on-change #(swap! state assoc :surname (.. % -target -value))}]]
+         {:on-change #(set-target-value state :surname %)}]]
        [:div.buttons
         [:input
          {:type "button"
           :value "Create"
-          :aria-disabled (invalid-input? @state)
+          :aria-disabled (not (can-create? @state))
           :on-click #(handle-create! state)}]
         [:input
          {:type "button"
           :value "Update"
-          :aria-disabled (or (nothing-selected? @state)
-                             (invalid-input? @state))
+          :aria-disabled (not (can-update? @state))
           :on-click #(handle-update! state)}]
         [:input
          {:type "button"
           :value "Delete"
-          :aria-disabled (nothing-selected? @state)
+          :aria-disabled (not (can-delete? @state))
           :on-click #(handle-delete! state)}]]])))
